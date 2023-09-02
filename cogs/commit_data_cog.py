2a68bfdf0340f3979f029e78a6a25eedf28414c5
HelloWorld183L <46497907+HelloWorld183L@users.noreply.github.com>
2023-09-02 18:20:39 +0100

    Added error checking + setup

--- cogs/commit_data_cog.py
+++ cogs/commit_data_cog.py
@@ -11,6 +11,11 @@ class CommitDataCog(discord.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
         self.data_folder = "./BSF-bot-data/weightcog/"
+        try:
+            subprocess.run(["git", "status"], check=True, capture_output=True)
+        except subprocess.CalledProcessError as e:
+            # Do we use logging instead?
+            ctx.send("Git isn't installed. Please install Git on the host.")
 
     @tasks.loop(time=commit_time)
     async def commit_data(self):
@@ -19,6 +24,14 @@ class CommitDataCog(discord.Cog):
         current_time = datetime_now.strftime('%H-%M-%S')
         
         commit_msg = f"'(UTC: {current_date} {current_time}) Committing user data'"
-        subprocess.run(["git", "add", self.data_folder])
+        try:
+            subprocess.run(["git", "add", self.data_folder])
+        except subprocess.CalledProcessError as e:
+            # Do we use logging instead?
+            ctx.send("Git could not find the data folder.")
+
         subprocess.run(["git", "commit", "-m", commit_msg])
         subprocess.run(["git", "push"])
+
+async def setup(bot):
+    await bot.add_cog(CommitDataCog(bot))
