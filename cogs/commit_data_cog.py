ff5da7ca304b76bc131782c4deb118ba656683cc
HelloWorld183L <karper360l@gmail.com>
2023-09-04 20:41:36 +0100

    Quick refactor before pull

--- cogs/commit_data_cog.py
+++ cogs/commit_data_cog.py
@@ -4,62 +4,54 @@ import subprocess
 import datetime
 import yaml
 import os
+from typing import Dict, Any
 
 class CommitDataCog(commands.Cog):
     """A discord cog that commits user data to the BSF-bot-data repository via Git"""
     timezone = datetime.timezone.utc
-    commit_time = datetime.time(hour=20, minute=30, tzinfo=timezone)
+    commit_time = datetime.time(hour=19, minute=40, tzinfo=timezone)
 
-    def __init__(self, bot: commands.Bot):
+    def __init__(self, bot: commands.Bot) -> None:
         self.bot = bot
         self.config_loc = "./BOT_CONFIG.yaml"
         config_yaml = self.get_config_yaml()
         self.data_folder = config_yaml["data-folder"]
-
+        self.commit_data.start()
         try:
             subprocess.run(["git", "--version"], check=True, capture_output=True)
         except subprocess.CalledProcessError as e:
-            # Do we use logging instead?
+            # TODO: Do we use logging instead?
             ctx.send("Git isn't installed. Please install Git on the host.")
 
     @commands.Cog.listener()
-    async def on_ready(self):
+    async def on_ready(self) -> None:
         print("Module: CommitDataCog")
 
     @commands.command()
-    async def start_commit_data(self, ctx):
+    async def start_commit_data(self, ctx: commands.Context) -> None:
         await self.commit_data()
 
     @tasks.loop(time=commit_time)
-    async def commit_data(self, ctx):
-        print("test0.5")
-        try:
-            datetime_now = datetime.datetime.now()
-            current_date = datetime_now.strftime('%Y-%m-%d')
-            current_time = datetime_now.strftime('%H-%M-%S')
-        except Exception as e:
-            print(e)
+    async def commit_data(self) -> None:
+        datetime_now = datetime.datetime.now()
+        current_date = datetime_now.strftime('%Y-%m-%d')
+        current_time = datetime_now.strftime('%H-%M-%S')
         
-        print("test1")
         commit_msg = f"'(UTC: {current_date} {current_time}) Committing user data'"
 
         try:
             os.chdir(self.data_folder)
-            subprocess.run(["git", "add", "."])
         except subprocess.CalledProcessError as e:
-            # Do we use logging instead?
             ctx.send("Git could not find the data folder.")
 
-        print("test2")
+        subprocess.run(["git", "add", "."])
         subprocess.run(["git", "commit", "-m", commit_msg])
-        print("test3")
         subprocess.run(["git", "push"])
-        subprocess.run(["ls"])
 
-    def get_config_yaml(self):
+    def get_config_yaml(self) -> Dict[str, Any]:
         with open(self.config_loc, 'r') as config_file:
             config_yaml = yaml.safe_load(config_file)
         return config_yaml
 
-async def setup(bot):
+async def setup(bot) -> None:
     await bot.add_cog(CommitDataCog(bot))
