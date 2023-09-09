e916976a7ad76d4ad26c4946a3f9727e720a4c9f
HelloWorld183L <karper360l@gmail.com>
2023-09-10 00:18:38 +0100

    Minor refactor
    Since the cog isn't finished, it won't be getting a full refactor

--- cogs/fitness_calculators.py
+++ cogs/fitness_calculators.py
@@ -2,13 +2,23 @@ import re
 import discord
 from discord.ext import commands
 
-class FitnessCalculators(commands.Cog):
-    def __init__(self, bot):
-        self.bot = bot
-        self.activity_keywords = ['cutting', 'bulking', 'maintaining']
+class FitnessCalculatorsCog(commands.Cog):
+    def __init__(self, bot : commands.Bot):
+        self.bot : commands.Bot = bot
+        self.activity_keywords : List[str] = ['cutting', 'bulking', 'maintaining']
+        self.gender_keywords : List[str] = ['male', 'female', 'gal', 'guy']
+        
+        # TODO: Can we reuse this conversion logic?
+        # Define regular expressions for each data element
+        self.height_regex: re.Pattern = re.compile(r'(\d+(\.\d+)?)\s*cm', re.IGNORECASE)
+        self.weight_regex: re.Pattern = re.compile(r'(\d+(\.\d+)?)\s*kg', re.IGNORECASE)
+        self.bf_regex: re.Pattern = re.compile(r'(\d+(\.\d+)?)\s*bf', re.IGNORECASE)
+        self.gender_regex: re.Pattern = re.compile('|'.join(self.gender_keywords), re.IGNORECASE)
+        self.age_regex: re.Pattern = re.compile(r'(\d+)\s*(?:years?|yo)', re.IGNORECASE)
+        self.activity_regex: re.Pattern = re.compile('|'.join(self.activity_keywords), re.IGNORECASE)
 
     @commands.command()
-    async def calculators(self, ctx):
+    async def calculators(self, ctx : commands.Context) -> None:
         await ctx.send(("To use the calculators simply type some numbers in a message "
                         "and the bot will figure out what to calculate. "
                         "\n\n"
@@ -21,16 +31,7 @@ class FitnessCalculators(commands.Cog):
                         ))
 
 
-    def extract_data(self, message_content):
-        # Define regular expressions for each data element
-        height_regex = re.compile(r'(\d+(\.\d+)?)\s*cm')
-        weight_regex = re.compile(r'(\d+(\.\d+)?)\s*kg')
-        bf_regex = re.compile(r'(\d+(\.\d+)?)\s*bf')
-        gender_regex = re.compile(r'(male|female|gal|guy)')
-        age_regex = re.compile(r'(\d+)\s*(?:years?|yo)')
-        activity_regex = re.compile('|'.join(self.activity_keywords), re.IGNORECASE)
-
-        # Initialize variables
+    def extract_data(self, message_content : str):
         height, weight, bodyfat, gender, age, activity = None, None, None, None, None, None
 
         # Extract data using regular expressions
@@ -43,19 +44,20 @@ class FitnessCalculators(commands.Cog):
         if bf_match := bf_regex.search(message_content):
             bodyfat = float(bf_match.group(1))
 
-        if gender_match := gender_regex.search(message_content.lower()):
+        message_content_lowered : str = message_content.lower()
+        if gender_match := gender_regex.search(message_content_lowered):
             gender = gender_match.group()
 
         if age_match := age_regex.search(message_content.lower()):
             age = int(age_match.group(1))
 
-        if activity_match := activity_regex.search(message_content.lower()):
+        if activity_match := activity_regex.search(message_content_lowered):
             activity = activity_match.group()
 
         return height, weight, bodyfat, gender, age, activity
 
     @commands.Cog.listener()
-    async def on_message(self, message):
+    async def on_message(self, message) -> None:
         # Check if the message is from a bot or not in a direct message
         #if message.author.bot or not message.guild:
         #    return
@@ -65,39 +67,42 @@ class FitnessCalculators(commands.Cog):
 
         # Check if two or more variables are filled
         filled_variables = [var for var in [height, weight, bodyfat, gender, age, activity] if var is not None]
-        if len(filled_variables) >= 2:
-            # Calculate statistics
-            bmi = None
-            ffmi = None
-            tdee = None
+        if len(filled_variables < 2):
+            return
 
-            if height and weight:
-                bmi = weight / (height ** 2)
+        # Calculate statistics
+        bmi = None
+        ffmi = None
+        tdee = None
 
-            if weight and bodyfat and height:
-                total_body_fat = weight * (bodyfat / 100)
-                lean_weight = weight - total_body_fat
-                ffmi = round((lean_weight / ((height /100) ** 2) + 6.1 * (1.8 - height / 100) ) / 10000,1)
+        if height and weight:
+            bmi = weight / (height ** 2)
 
-            if height and weight and gender and activity:
-                if activity.lower() == 'cutting':
-                    tdee = 10 * weight + 6.25 * height - 5 * age - 161
-                elif activity.lower() == 'bulking':
-                    tdee = 10 * weight + 6.25 * height - 5 * age + 5
-                elif activity.lower() == 'maintaining':
-                    tdee = 10 * weight + 6.25 * height - 5 * age
+        if weight and bodyfat and height:
+            total_body_fat = weight * (bodyfat / 100)
+            lean_weight = weight - total_body_fat
+            ffmi = round((lean_weight / ((height /100) ** 2) + 6.1 * (1.8 - height / 100) ) / 10000,1)
 
-            # Create a response message with the extracted data and calculated statistics
-            #response = f"Height (cm): {height * 100}, Weight (kg): {weight}, Bodyfat (%): {bodyfat}, Gender: {gender}, Age: {age}, Activity: {activity}\n"
-            response = ""
-            if bmi is not None:
-                response += f"BMI: {bmi:.2f}\n"
-            if ffmi is not None:
-                response += f"FFMI: {ffmi:.2f}\n"
-            if tdee is not None:
-                response += f"TDEE: {tdee:.2f}\n"
+        if height and weight and gender and activity:
+            initial_tdee = 10 * weight + 6.25 * height - 5 * age
+            if activity.lower() == 'cutting':
+                tdee = initial_tdee - 161
+            elif activity.lower() == 'bulking':
+                tdee = initial_tdee + 5
+            elif activity.lower() == 'maintaining':
+                tdee = initial_tdee
 
-            await message.channel.send(response)
+        # Create a response message with the extracted data and calculated statistics
+        #response = f"Height (cm): {height * 100}, Weight (kg): {weight}, Bodyfat (%): {bodyfat}, Gender: {gender}, Age: {age}, Activity: {activity}\n"
+        response : str = ""
+        if bmi is not None:
+            response += f"BMI: {bmi:.2f}\n"
+        if ffmi is not None:
+            response += f"FFMI: {ffmi:.2f}\n"
+        if tdee is not None:
+            response += f"TDEE: {tdee:.2f}\n"
 
-async def setup(bot):
+        await message.channel.send(response)
+
+async def setup(bot : commands.Bot) -> None:
     await bot.add_cog(FitnessCalculators(bot))
