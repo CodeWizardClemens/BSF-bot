ae01fb7cba1afede75e2c64df952b404c62183a0
HelloWorld183L <karper360l@gmail.com>
2023-09-10 15:14:13 +0100

    Refactored WorkoutAssistantCog

--- cogs/workout_assistant_cog.py
+++ cogs/workout_assistant_cog.py
@@ -8,9 +8,7 @@ from discord.ext import commands
 import os
 import spacy
 
-
-
-class WorkoutAssistant(commands.Cog):
+class WorkoutAssistantCog(commands.Cog):
     """A Discord cog for replying to 'source that' message and displaying the content of the most relevant file."""
 
     def __init__(self, bot: commands.Bot):
@@ -25,20 +23,21 @@ class WorkoutAssistant(commands.Cog):
             message (discord.Message): The message sent by a user.
 
         """
-        if message.author == self.bot.user:
+        if message.author == self.bot.user or not 'volume' in message.content.lower():
             return
 
-        if 'volume' in  message.content.lower():
-            replied_message = message.reference.resolved if message.reference else None
-            if replied_message:
-                min_sets_per_week, max_sets_per_week = VolumeCalculator.get_sets_per_week(replied_message.content)
-                if min_sets_per_week == max_sets_per_week:
-                    await message.channel.send(f"Sets in this program: {min_sets_per_week}")
-                    return
-                else:
-                    await message.channel.send((f"Minimum sets: {min_sets_per_week}\n"
-                                               f"Maximum sets: {max_sets_per_week}"))
-                    return
+        replied_message = message.reference.resolved if message.reference else None
+        if not replied_message:
+            return
+
+        min_sets_per_week, max_sets_per_week = VolumeCalculator.get_sets_per_week(replied_message.content)
+        if min_sets_per_week == max_sets_per_week:
+            await message.channel.send(f"Sets in this program: {min_sets_per_week}")
+            return
+        else:
+            await message.channel.send((f"Minimum sets: {min_sets_per_week}\n"
+                                        f"Maximum sets: {max_sets_per_week}"))
+            return
 
 async def setup(bot: commands.Bot):
     """Setup function to add the SourceCog cog to the bot.
@@ -47,4 +46,4 @@ async def setup(bot: commands.Bot):
         bot (commands.Bot): The bot instance.
 
     """
-    await bot.add_cog(WorkoutAssistant(bot))
+    await bot.add_cog(WorkoutAssistantCog(bot))
