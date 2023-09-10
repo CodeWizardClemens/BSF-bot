7a2544eb5898876fda2c94c665a974d91c3cd73f
HelloWorld183L <karper360l@gmail.com>
2023-09-10 01:10:40 +0100

    Renamed+Refactored polls_cog

---
+++ cogs/polls_cog.py
@@ -0,0 +1,25 @@
+import discord
+from discord.ext import commands
+import random
+import json
+
+class PollsCog(commands.Cog):
+    def __init__(self, bot):    
+        self.bot = bot
+        self.polls_channel_id : int = 962433889081626624
+        self.thumbs_up_emoji : str = '\N{THUMBS UP SIGN}'
+        self.thumbs_down_emoji : str = '\N{THUMBS DOWN SIGN}'
+
+    @commands.Cog.listener()
+    async def on_ready(self) -> None:
+        print("Module: Polls")
+
+    @commands.Cog.listener()
+    async def on_message(self, message) -> None:
+        print(message.channel.id)
+        if(message.channel.id == self.polls_channel_id):
+            await message.add_reaction(self.thumbs_up_emoji)
+            await message.add_reaction(self.thumbs_down_emoji)
+
+async def setup(bot) -> None:
+    await bot.add_cog(PollsCog(bot))
