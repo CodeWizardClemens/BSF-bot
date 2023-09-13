f19d8ffde04620df984628eea53035acac88d1e7
HelloWorld183L <46497907+HelloWorld183L@users.noreply.github.com>
2023-09-13 18:01:40 +0100

    Update polls.py from refactor/cogs

--- cogs/polls.py
+++ cogs/polls.py
@@ -2,23 +2,29 @@ import discord
 from discord.ext import commands
 import random
 import json
+from typing import Final, Dict
 
-class Polls(commands.Cog):
-    
-    def __init__(self, client):    
-        self.client = client
+class PollsCog(commands.Cog):
+    POLLS_CHANNEL_ID : Final[int] = 962433889081626624
+    EMOJIS : Final[Dict[str,str]] = {
+        "thumbs_up": '\N{THUMBS UP SIGN}',
+        "thumbs_down": '\N{THUMBS DOWN SIGN}'
+    }
+
+    def __init__(self, bot):    
+        self.bot = bot
 
     @commands.Cog.listener()
-    async def on_ready(self):
+    async def on_ready(self) -> None:
         print("Module: Polls")
 
     @commands.Cog.listener()
-    async def on_message(self, message):
+    async def on_message(self, message) -> None:
+        # Creates poll based off thumbs up/down emojis
+        if(message.channel.id == PollsCog.POLLS_CHANNEL_ID):
+            await message.add_reaction(PollsCog.EMOJIS["thumbs_up"])
+            await message.add_reaction(PollsCog.EMOJIS["thumbs_down"])
 
-        print(message.channel.id)
-        if(message.channel.id == 962433889081626624):
-            await message.add_reaction('\N{THUMBS UP SIGN}')
-            await message.add_reaction('\N{THUMBS DOWN SIGN}')
+async def setup(bot) -> None:
+    await bot.add_cog(PollsCog(bot))
 
-async def setup(client):
-    await client.add_cog(Polls(client))
