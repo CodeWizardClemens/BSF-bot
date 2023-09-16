ffdee2db7e2c053e2864f38d3edac58828bd64a3
HelloWorld183L <46497907+HelloWorld183L@users.noreply.github.com>
2023-09-16 16:12:04 +0100

    Update funny_reactions.py from refactor/cogs (#23)

--- cogs/funny_reactions.py
+++ cogs/funny_reactions.py
@@ -1,25 +1,34 @@
 import discord
 from discord.ext import commands
-import random
-import json
+from typing import List, Final, Dict
 
-class FunnyReactions(commands.Cog):
-    
-    def __init__(self, client):    
-        self.client = client
+class FunnyReactionsCog(commands.Cog):
+    """
+    Discord cog that reacts to messages with buzzwords using funny emojis
+    """
+
+    BUZZWORDS : Final[List[str]] = ['eddie','water','thor','fart','strong','deadlift']
+    REACTIONS : Final[Dict[str, str]] = {
+        "eddieleftarm": '<:eddiearm:794302033523245075>',
+        "eddierightarm": '<:armeddie:794302066201853962>',
+        "eddieshitting": '<:eddieshitting:794198092525338654>'
+    }
+    def __init__(self, bot):    
+        self.bot = bot
 
     @commands.Cog.listener()
-    async def on_ready(self):
+    async def on_ready(self) -> None:
         print("Module: FunnyReactions")
 
     @commands.Cog.listener()
-    async def on_message(self, message):
-        buzzwords = ['eddie','water','thor','fart','strong','deadlift']
-        for word in buzzwords:
-            if word in message.content.lower():
-                await message.add_reaction('<:eddiearm:794302033523245075>')
-                await message.add_reaction('<:eddieshitting:794198092525338654>')
-                await message.add_reaction('<:armeddie:794302066201853962>')
+    async def on_message(self, message : discord.Message) -> None:
+        # TODO: Do we need to loop through all the different buzzwords if they all have the same response?
+        lowercase_content = message.content.lower()
+        for word in FunnyReactionsCog.BUZZWORDS:
+            if word in lowercase_content:
+                await message.add_reaction(FunnyReactionsCog.REACTIONS["eddieleftarm"])
+                await message.add_reaction(FunnyReactionsCog.REACTIONS["eddieshitting"])
+                await message.add_reaction(FunnyReactionsCog.REACTIONS["eddierightarm"])
 
-async def setup(client):
-    await client.add_cog(FunnyReactions(client))
+async def setup(bot) -> None:
+    await bot.add_cog(FunnyReactionsCog(bot))
