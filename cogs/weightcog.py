aaff6e62107c89ca7bd376e6b7ab4802056506cb
HelloWorld183L <karper360l@gmail.com>
2023-09-19 14:30:23 +0100

    AMMEND Refactored + added documentation

--- cogs/weightcog.py
+++ cogs/weightcog.py
@@ -9,24 +9,49 @@ from typing import Dict, List, Final, Any
 from pathlib import Path
 import yaml
 
+
+"""
+Discord cog module that stores, reads and removes weight data.
+This cog can be loaded using an extension.
+"""
+
 def has_bot_input_perms(ctx):
+    """
+    Checks if the user has the bot-input role.
+    """
+
     role = discord.utils.get(ctx.guild.roles, name="bot-input")
     return role in ctx.author.roles
 
 class WeightCog(commands.Cog):
+    """
+    Discord cog that stores, reads and removes weight data. 
+    It also can make time series plots that record weight change over time.
+    """
+
     MOVING_AVG_PERIODS : Final[Dict[str, int]] = {
         "weekly_avg": 7, "last_week": 7,
         "monthly_avg": 30, "last_month": 30,
         "yearly_avg": 365, "last_year": 365
     }
-    # Header row of a user weight csv file
+    """
+    Moving average period types and their corresponding integer values.
+    """
+
     HEADER_ROW : Final[List[str]] = ["Date", "Weight"] 
+    """
+    Header row of a user weight csv file.
+    """
+
+    CONFIG_PATH: Final[str] = Path("config.yaml")
+    """
+    The bot config path
+    """
     
     def __init__(self, bot : commands.Bot):
-        self.bot = bot
-        self.CONFIG_PATH: Final[str] = Path("./BOT_CONFIG.yaml")
-        self.config : Dict[str, Any] = self.get_config()
-        self.WEIGHT_COG_DATA_PATH : Final[str] = self.config["weight-cog-data-path"]
+        self.BOT = bot
+        self.CONFIG : Final[Dict[str, Any]] = self.get_config()
+        self.WEIGHT_COG_DATA_PATH : Final[str] = self.CONFIG["weight-cog-data-path"]
 
     @commands.command()
     async def weight_goal(self, ctx : commands.Context, weight: float, user: discord.Member, date: str = None) -> None:
@@ -39,9 +64,9 @@ class WeightCog(commands.Cog):
         .weight 75.5 2023-08-27 @user
 
         Parameters:
-        :param weight: (float) The weight value to be recorded.
-        :param date: (str, optional) The date of the weight entry (default: current date).
-        :param user: (discord.Member, optional): The user for whom the weight is being recorded (default: yourself).
+            weight: (float): The weight value to be recorded.
+            date: (str, optional): The date of the weight entry (default: current date).
+            user: (discord.Member, optional): The user for whom the weight is being recorded (default: yourself).
         """
     
         if user and (user != ctx.author) and not has_bot_input_perms(ctx):
@@ -59,26 +84,26 @@ class WeightCog(commands.Cog):
             date : str = str(ctx.message.created_at.date())
     
         user_weight_path : str = os.path.join(self.WEIGHT_COG_DATA_PATH, f"{user_id}_goal_weight.csv")
-
+        # Insert weight entry and sorts the new weight entries
         new_entries = self.insert_weight_entry(date, weight, user_weight_path)
         self.sort_weight_entries(user_weight_path, new_entries)
 
         await ctx.send(f"Weight goal recorded for {date} ({user.display_name}): {weight} kg.")
 
     @commands.command()
-    async def weight(self, ctx, weight: float, user: discord.Member = None, date: str = None):
+    async def weight(self, ctx, weight: float, user: discord.Member = None, date: str = None) -> None:
         """
-        Records a user's weight
+        Records a user's weight.
 
         Usage:
         .weight <weight> [user] [date]
         Example:
         .weight 75.5 @user 2023-08-27
 
-        Parameters:
-        :param weight: (float) The weight value to be recorded.
-        :param user: (discord.Member, optional): The user for whom the weight is being recorded (default: yourself).
-        :param date: (str, optional) The date of the weight entry (default: current date).
+        Args:
+            weight: (float) The weight value to be recorded.
+            user: (discord.Member, optional): The user for whom the weight is being recorded (default: yourself).
+            date: (str, optional) The date of the weight entry (default: current date).
 
         """
         if user and (user != ctx.author) and not has_bot_input_perms(ctx):
@@ -99,12 +124,17 @@ class WeightCog(commands.Cog):
 
         new_entries = self.insert_weight_entry(date, weight, user_weight_path)
         self.sort_weight_entries(user_weight_path, new_entries)
+
         await ctx.send(f"Weight recorded for {date} ({user.display_name}): {weight} kg.")
 
     # TODO: Put this into WeightRepository
     def sort_weight_entries(self, user_weight_path : str, entries) -> None:
         """
-        Sorts the weight entries by date and overwrites the CSV file with the sorted entries
+        Sorts the weight entries by date and overwrites the CSV file with the sorted entries.
+
+        Args:
+            user_weight_path (str): The path to the user's weight data.
+            entries (): The current weight entries that a user has.
         """
         # Sort entries by date
         entries.sort(key=lambda entry: entry[0])
@@ -116,9 +146,16 @@ class WeightCog(commands.Cog):
             csv_writer.writerows(entries)
 
     # TODO: Put this into WeightRepository
-    def insert_weight_entry(self, date : date, weight : float, user_weight_path):
+    def insert_weight_entry(self, date : date, weight : float, user_weight_path : str):
         """
-        Reads the existing entries in the user_weight_path and inserts a new weight entry
+        Reads the existing entries in the user_weight_path and inserts a new weight entry.
+
+        Args:
+            date (date): The date for weight entry
+            weight (float): The new weight for the weight entry
+            user_weight_path (str): The path for the user's weight data
+
+        Returns the new entries
         """
         # Read existing entries
         entries = []
@@ -156,8 +193,14 @@ class WeightCog(commands.Cog):
     def read_weight_data(self, user_weight_path : str, period):
         """
         Reads all of the user weight data relevant inside a period
+
+        Args:
+            user_weight_path (str): The path to the user's weight data
+            period (): The period to read the weight data inside of (e.g. week, month, year)
+
+        Returns the user's weight data from a period
         """
-        user_weight_data = []
+        user_weight_data_from_period = []
         with open(user_weight_path, "r") as csvfile:
             csv_reader : csv.reader = csv.reader(csvfile)
             # Skips the header row
@@ -166,22 +209,29 @@ class WeightCog(commands.Cog):
                 if row == WeightCog.HEADER_ROW: continue
                 date : date = datetime.strptime(row[0], "%Y-%m-%d").date()
                 if self.date_inside_period(period, date):
-                    weight : float = float(row[1])
-                    user_weight_data.append((date, weight))
+                    weight = float(row[1])
+                    user_weight_data_from_period.append((date, weight))
 
-        return user_weight_data
+        return user_weight_data_from_period
 
-    """
-    Creates a time series line plot of how weight increases over time with the option to create a moving average line
-    """
     def create_weight_plot(self, dates, weights, user, moving_averages = None, moving_avg_dates = None) -> None:
-        has_moving_averages = moving_averages != None and moving_avg_dates != None
+        """
+        Creates a time series line plot of how weight increases over time with the option to create a moving average line.
+
+        Args:
+            dates (): The dates extracted from the weight entries.
+            weights (): The weights extracted from the weight entries.
+            user (): The details of the user (we use the display name).
+            moving_averages (| None): The moving averages for each weight data point.
+            moving_avg_dates (| None): The moving average dates for each weight entry.
+        """
+        has_moving_averages : bool = moving_averages != None and moving_avg_dates != None
 
         plt.figure(figsize=(10, 6))
         # Weight data
         plt.plot(dates, weights, marker='o', color='red', linewidth=3)
         if has_moving_averages:
-            # Moving average
+            # Moving average data
             plt.plot(moving_avg_dates, moving_averages, color='white', linewidth=3, label='Moving Average')
 
         plt.xlabel("Date", fontsize=16, color='white')
@@ -204,7 +254,15 @@ class WeightCog(commands.Cog):
     def remove_weight_record(self, user_weights_path : str, temp_user_weights_path : str, date : date) -> bool:
         """
         Removes the weight record for a user on a specific date
+
+        Args:
+            user_weights_path (str): Path for a user's weight data
+            temp_user_weights_path (str): Temporary path for a user's weight data. We then use this to replace the original weight path
+            date (date): The date of the weight record to remove
+
+        Returns the boolean result of whether the weight record was found and removed.
         """
+
         removed = False
         with open(user_weights_path, "r") as input_file, open(temp_user_weights_path, "w", newline="") as output_file:
             csv_reader : csv.reader = csv.reader(input_file)
@@ -219,11 +277,11 @@ class WeightCog(commands.Cog):
         os.replace(temp_user_weights_path, user_weights_path)
         return removed
 
-    """
-    Gets the config file contents that contain the data folder path
-    """
     def get_config(self) -> Dict[str, Any]:
-        with open(self.CONFIG_PATH, 'r') as config_file:
+        """
+        Gets the config file contents that contain the data folder path
+        """
+        with open(WeightCog.CONFIG_PATH, 'r') as config_file:
             return yaml.safe_load(config_file)
 
     @commands.command()
@@ -234,7 +292,7 @@ class WeightCog(commands.Cog):
                 description="Options: weekly_avg, monthly_avg, yearly_avg, no_avg."),
             period: str = commands.parameter(
                 default="last_month",
-                description="Which data to dosplay. Options: last_week, last_month, last_year, all"),
+                description="Which data to display. Options: last_week, last_month, last_year, all"),
             user: discord.Member = commands.parameter(
                 default=None,
                 description="A mention to a discord user."),
@@ -245,9 +303,11 @@ class WeightCog(commands.Cog):
         This requires Qt platform plugin "wayland".
 
         Usage:
+        
         .stats <moving_average> <period> <user>
 
         Examples:
+
         1. Display your stats:
         .stats
 
@@ -256,6 +316,10 @@ class WeightCog(commands.Cog):
 
         3. Display the weekly average for a user for the weight they tracked last year.
         .stats weekly_avg last_year @username
+
+        Args:
+            moving_average (str): Moving average period specified (or no_avg)
+            period (str): Period of data to display. This is used for displaying stats WITHOUT a moving average
         """
 
         if user and (user != ctx.author) and not has_bot_input_perms(ctx):
@@ -305,8 +369,9 @@ class WeightCog(commands.Cog):
         """
         Remove a specific weight entry.
 
-        :param date: (str): The date of the weight entry to be removed.
-        :param user: (discord.Member, optional): The user for whom the weight entry should be removed (default: yourself).
+        Args:
+            date: (str): The date of the weight entry to be removed.
+            user: (discord.Member, optional): The user for whom the weight entry should be removed (default: yourself).
 
         Usage:
         .remove_weight <date> [user]
@@ -317,7 +382,6 @@ class WeightCog(commands.Cog):
         if user and (user != ctx.author) and not has_bot_input_perms(ctx):
             await ctx.send(f"You don't have the bot-input role and are therefore nore allowed to specify other users")
             return
-
 
         user = user or ctx.author
         user_id : str = str(user.id)
@@ -336,12 +400,14 @@ class WeightCog(commands.Cog):
 
     # TODO: This code can be put into WeightRepository
     @commands.command()
-    async def export(self, ctx, user: discord.Member = None) -> None:
+    async def export(self, ctx : commands.Context, user: discord.Member = None) -> None:
         """
-        Export weight data as a CSV file.
+        Export the specified user's weight data as a CSV file.
+        Only people with the `bot-input` role can specify other users
     
-        Parameters:
-        - user (discord.Member, optional): The user for whom the weight data should be exported (default: yourself).
+        Args:
+            ctx (commands.Context): The command context
+            user (discord.Member, optional): The user for whom the weight data should be exported (default: yourself).
     
         Usage:
         .export [user]
@@ -372,7 +438,8 @@ class WeightCog(commands.Cog):
             await ctx.send(f"Weight data for {user.display_name}", file=csv_attachment)
 
 async def setup(bot: commands.Bot) -> None:
-    """Setup function to add the ConversionCog cog to the bot.
+    """
+    Setup function to add the ConversionCog cog to the bot.
 
     Args:
         bot (commands.Bot): The bot instance.
