d9f32991fa44b06b546113fd00ea4ea520714b67
CodeWizardClemens <143846829+CodeWizardClemens@users.noreply.github.com>
2023-10-10 19:20:35 +0200

    Add various test cases (#47)
    * Add various test cases
    
    * Added extra test cases
    
    * Added FunnyReactionsCog tests
    
    * Added PollsCog tests
    
    * Tweak or skip test cases so they pass
    
    ---------
    
    Co-authored-by: HelloWorld183L <karper360l@gmail.com>

--- tests/test_fitness_calculators.py
+++ tests/test_fitness_calculators.py
@@ -8,7 +8,6 @@ from libs.tester_slave import TesterSlaveCog, TesterSlaveEnvironment
 This module contains the test cases for the conversion cog.
 """
 
-
 @pytest.fixture
 def tester_slave_environment() -> TesterSlaveEnvironment:
     """
@@ -18,22 +17,17 @@ def tester_slave_environment() -> TesterSlaveEnvironment:
     """
     return TesterSlaveEnvironment()
 
-
 @pytest.mark.asyncio
 async def test_conversion_cog(tester_slave_environment: TesterSlaveEnvironment):
     """
     Test the conversion cog.
     """
 
-    # FIXME the height conversions seem to be broken.
+    # FIXME The BMR calculator seems to be broken.
     test_values = [
-        ("I am 200 lbs", "I am 90.7 kg"),
-        ("I am 200lbs", "I am 90.7 kg"),
-        ("I am 200 pounds", "I am 90.7 kg"),
-        ("I am 0lbs", "I am 0.0 kg"),
-        ("I am -10lbs", "I am -4.5 kg"),
-        ("I am 200lbs and my cousin is 150lbs", "I am 90.7 kg and my cousin is 68.0 kg"),
-        ("I am 200lbs and I am 5'10", "I am 90.7 kg and I am 178 cm "),
+        ("184cm 99kg", "BMI: 29.24"),
+        ("100kg 100cm", "BMI: 100.00"),
+        ("30bf 188cm 120kg", "BMI: 33.95\nFFMI: 23.80"),
     ]
 
     async def steps(self: TesterSlaveCog):
@@ -48,5 +42,5 @@ async def test_conversion_cog(tester_slave_environment: TesterSlaveEnvironment):
 
     for responce_index, test_value in enumerate(test_values):
         assert (
-            tester_slave_environment.responces[responce_index].content == f"```{test_value[1]}```"
+            tester_slave_environment.responces[responce_index].content == test_value[1]
         )
