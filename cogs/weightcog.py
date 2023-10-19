b22abec2faf6d0c7d6065b0c2fb83789a3eee7a3
CodeWizardClemens <143846829+CodeWizardClemens@users.noreply.github.com>
2023-10-19 20:32:39 +0200

    Fix f-string without any placeholders (#56)

--- cogs/weightcog.py
+++ cogs/weightcog.py
@@ -608,7 +608,7 @@ class WeightCog(commands.Cog):
             if option.emoji == "✅":
                 os.remove(file_path)
                 embed = discord.Embed(
-                    title=f"All logs have been deleted",
+                    title="All logs have been deleted",
                     timestamp=datetime.utcnow(),
                     colour=discord.Colour.blurple(),
                 )
@@ -616,7 +616,7 @@ class WeightCog(commands.Cog):
                 return
             else:
                 embed = discord.Embed(
-                    title=f"Operation Cancelled",
+                    title="Operation Cancelled",
                     timestamp=datetime.utcnow(),
                     colour=discord.Colour.blurple(),
                 )
