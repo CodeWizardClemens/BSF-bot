cf7e5679effa0acbe92e8c3f3d03ee4a608f2e22
Sarthak singh <sarthaksingh2001@gmail.com>
2023-10-31 22:15:52 +0530

    added postgresql db

--- BSF-bot.py
+++ BSF-bot.py
@@ -1,17 +1,63 @@
 #!/usr/bin/env python3
 
 import asyncio
+import logging
 import os
 from pathlib import Path
 from typing import Final
 
 import discord
+import asyncpg
 import yaml
 from discord.ext import commands
+from models.model import Model
+
 
 TOKEN: Final[str] = yaml.safe_load(Path("discord_token.yaml").open())["discord_token"]
+URI: Final[str] = yaml.safe_load(Path("discord_token.yaml").open())["uri"]
+
 
 client = commands.Bot(command_prefix=".", intents=discord.Intents.all())
+log = logging.getLogger(__name__)
+
+
+async def prepare_postgres(postgres_uri: str, retries: int = 5, interval: float = 5.0, **cp_kwargs) -> bool:
+    """
+    Prepare the postgres database connection.
+
+    :param str postgres_uri:  The postgresql URI to connect to.
+    :param int retries:       Included to fix issue with docker starting API before DB is finished starting.
+    :param float interval:    Interval of which to wait for next retry.
+    """
+
+    db_name = postgres_uri.split("/")[-1]
+    db_log = logging.getLogger("DB")
+
+    if Model.pool is not None:
+        db_log.info("No need to create pool, it already exists!")
+        return True
+
+    db_log.info(f'Attempting to connect to DB "{db_name}"')
+    for i in range(1, retries + 1):
+        try:
+            await Model.create_pool(uri=postgres_uri, **cp_kwargs)
+            break
+
+        except asyncpg.InvalidPasswordError as e:
+            db_log.error(str(e))
+            return False
+
+        except (ConnectionRefusedError,):
+            db_log.warning(f"Failed attempt #{i}/{retries}, trying again in {interval}s")
+
+            if i == retries:
+                db_log.error("Failed final connection attempt, exiting.")
+                return False
+
+            await asyncio.sleep(interval)
+
+    db_log.info(f'Connected to database "{db_name}"')
+    return True
 
 
 @client.command(brief="Load clog module")
@@ -49,6 +95,14 @@ async def load_extensions():
 
 
 async def main():
+    discord.utils.setup_logging()
+    if not await prepare_postgres(
+            URI,
+            max_con=10,
+            min_con=1,
+    ):
+        log.info("Not Connected to DB")
+
     async with client:
         await load_extensions()
         await client.start(TOKEN)
